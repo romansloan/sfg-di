8c2200aac78fce7702cea61aab5f3430387ca96d
roman.a.sloan@gmail.com <roman.a.sloan@gmail.com>
2020-06-21 01:28:52 -0400

    Added MyController. Tested ApplicationContext in SfgDiApplication.java

---
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
